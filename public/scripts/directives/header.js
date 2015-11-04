9ca926d849e9f8c3ad6edc9ae78e4417a04494c0
JANINE <nicegal17@gmail.com>
2015-11-04 18:16:02 +0800

    Remove Some files in public

--- public/scripts/directives/header.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('accounting')
-    .directive('navBar', function() {
-        return {
-            restrict: 'AE',
-            templateUrl: 'templates/directives/header.html'
-        };
-    });
